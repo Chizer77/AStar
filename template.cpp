89881f8730a1eead2386a3750feb50241bf0454b
Chizer77 <97499725+Chizer77@users.noreply.github.com>
2025-05-23 03:31:15 +0800

    init

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define rep(i, j ,k) for(int i = (j); i <= (k); ++i)
+#define dep(i, j, k) for(int i = (j); i >= (k); --i)
+using i64 = int64_t;
+using f64 = double_t;
+#define pb push_back
+#define ll long long
+#define vi vector <int>
+#define sz(a) ((int) (a).size())
+#define me(f, x) memset(f, x, sizeof(f))
+#define mod 1000000007
+#define inf 0x3f3f3f3f
+#define maxn 100005
+using namespace std;
+
+int T;
+
+void solve() {
+    
+}
+
+int main () {
+    ios:: sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    T = 1;
+    // cin >> T;
+    while(T--) solve();
+}
